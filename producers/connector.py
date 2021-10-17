880f11de262e23ef7717930785afded38d0f239f
Mikhail Setkin <msetkin@yandex.ru>
2021-10-17 23:10:30 +0300

    done kafka connect from PG task

--- producers/connector.py
+++ producers/connector.py
@@ -1,4 +1,5 @@
 """Configures a Kafka Connector for Postgres Station data"""
+import sys, os
 import json
 import logging
 
@@ -6,10 +7,10 @@ import requests
 
 
 logger = logging.getLogger(__name__)
-
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 KAFKA_CONNECT_URL = "http://localhost:8083/connectors"
-CONNECTOR_NAME = "stations"
+CONNECTOR_NAME = "com.streaming.pg.cta.stations"
 
 def configure_connector():
     """Starts and configures the Kafka Connect connector"""
@@ -17,58 +18,47 @@ def configure_connector():
 
     resp = requests.get(f"{KAFKA_CONNECT_URL}/{CONNECTOR_NAME}")
     if resp.status_code == 200:
-        logging.debug("connector already created skipping recreation")
+        logging.info("connector already created skipping recreation")
         return
 
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-        return
-
-    # TODO: Complete the Kafka Connect Config below.
-    # Directions: Use the JDBC Source Connector to connect to Postgres. Load the `stations` table
-    # using incrementing mode, with `stop_id` as the incrementing column name.
-    # Make sure to think about what an appropriate topic prefix would be, and how frequently Kafka
-    # Connect should run this connector (hint: not very often!)
-    logger.info("connector code not completed skipping connector creation")
-    #resp = requests.post(
-    #    KAFKA_CONNECT_URL,
-    #    headers={"Content-Type": "application/json"},
-    #    data=json.dumps({
-    #        "name": CONNECTOR_NAME,
-    #        "config": {
-    #            "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
-    #            "key.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "key.converter.schemas.enable": "false",
-    #            "value.converter": "org.apache.kafka.connect.json.JsonConverter",
-    #            "value.converter.schemas.enable": "false",
-    #            "batch.max.rows": "500",
-    #            # TODO
-    #            "connection.url": "",
-    #            # TODO
-    #            "connection.user": "",
-    #            # TODO
-    #            "connection.password": "",
-    #            # TODO
-    #            "table.whitelist": "",
-    #            # TODO
-    #            "mode": "",
-    #            # TODO
-    #            "incrementing.column.name": "",
-    #            # TODO
-    #            "topic.prefix": "",
-    #            # TODO
-    #            "poll.interval.ms": "",
-    #        }
-    #    }),
-    #)
+    resp = requests.post(
+        KAFKA_CONNECT_URL,
+        headers={"Content-Type": "application/json"},
+        data=json.dumps( 
+            {
+                "name": CONNECTOR_NAME,
+                "config": {
+                    "connector.class": "io.confluent.connect.jdbc.JdbcSourceConnector",
+                    "key.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "key.converter.schemas.enable": "false",
+                    "value.converter": "org.apache.kafka.connect.json.JsonConverter",
+                    "value.converter.schemas.enable": "false",
+                    "batch.max.rows": "500",
+                    "connection.url": "jdbc:postgresql://postgres:5432/cta",
+                    "connection.user": os.environ['PG_USER'],
+                    "connection.password": os.environ['PG_PASSWORD'],
+                    "table.whitelist": "stations",
+                    "mode": "incrementing",
+                    "incrementing.column.name": "stop_id",
+                    "topic.prefix": "com.streaming.connect.",
+                    "poll.interval.ms": 10000,
+                }
+            }
+        ),
+    )
 
     ## Ensure a healthy response was given
-    #resp.raise_for_status()
-    #logging.debug("connector created successfully")
+    try:
+        resp.raise_for_status()
+    except:
+        logger.error(f"Failed to send data to REST Proxy {json.dumps(resp.json(), indent=2)}")
+        return
+    
+    logging.info("connector created successfully")
+
+def main():
+    configure_connector()
 
 
 if __name__ == "__main__":
-    configure_connector()
+    main()
