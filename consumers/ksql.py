455e1c0b8ae1156ebd523150a90ff1fb33c06658
Mikhail Setkin <msetkin@yandex.ru>
2021-10-18 13:18:44 +0300

    done ksql task

--- consumers/ksql.py
+++ consumers/ksql.py
@@ -8,35 +8,65 @@ import topic_check
 
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
 
 
 KSQL_URL = "http://localhost:8088"
 
 #
+# Next, we will use KSQL to aggregate turnstile data for each of our stations.
+# Recall that when we produced turnstile data, we simply emitted an event, not a count. 
+# What would make this data more useful would be to summarize it by station so that 
+# downstream applications always have an up-to-date count
+#
 # TODO: Complete the following KSQL statements.
 # TODO: For the first statement, create a `turnstile` table from your turnstile topic.
 #       Make sure to use 'avro' datatype!
-# TODO: For the second statment, create a `turnstile_summary` table by selecting from the
+# TODO: For the second statement, create a `turnstile_summary` table by selecting from the
 #       `turnstile` table and grouping on station_id.
 #       Make sure to cast the COUNT of station id to `count`
 #       Make sure to set the value format to JSON
 
 KSQL_STATEMENT = """
-CREATE TABLE turnstile (
-    ???
+CREATE STREAM com_streaming_ksql_turnstile_stream (
+    line VARCHAR,
+    station_id INT,
+    station_name VARCHAR,
+    num_entries INT
 ) WITH (
-    ???
+    KAFKA_TOPIC = 'com.streaming.produce.turnstile',
+    VALUE_FORMAT = 'Avro'
 );
 
-CREATE TABLE turnstile_summary
-WITH (???) AS
-    ???
-"""
+CREATE TABLE com_streaming_ksql_turnstile_table (
+    line VARCHAR,
+    station_id INT,
+    station_name VARCHAR,
+    num_entries INT
+) WITH (
+    KAFKA_TOPIC = 'com.streaming.produce.turnstile',
+    VALUE_FORMAT = 'Avro',
+    KEY = 'station_id'
+);
 
+CREATE TABLE com_streaming_ksql_turnstile_summary_table
+WITH (
+    VALUE_FORMAT = 'JSON'
+)
+AS
+    SELECT station_id,
+           sum (num_entries) as sum_num_entries,
+           count (1) as cnt_entries
+      FROM com_streaming_ksql_turnstile_stream
+    WINDOW HOPPING (SIZE 10 MINUTES, ADVANCE BY 1 MINUTE)
+     GROUP BY station_id;
+"""
 
 def execute_statement():
     """Executes the KSQL statement against the KSQL API"""
     if topic_check.topic_exists("TURNSTILE_SUMMARY") is True:
+        logging.info(f"topic {TURNSTILE_SUMMARY} already exists. Executing ksql statement...")
         return
 
     logging.debug("executing ksql statement...")
@@ -53,7 +83,13 @@ def execute_statement():
     )
 
     # Ensure that a 2XX status code was returned
-    resp.raise_for_status()
+    try:
+        resp.raise_for_status()
+    except:
+        logger.error(f"Failed to send data to REST Proxy {json.dumps(resp.json(), indent=2)}")
+        return
+    
+    logging.info("KSQL statement executed successfully")
 
 
 if __name__ == "__main__":
