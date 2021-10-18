47030e6c20ecf8c47a84e7d5e7998cab82fe00a9
Mikhail Setkin <msetkin@yandex.ru>
2021-10-18 10:23:37 +0300

    changed topic name

--- producers/models/turnstile.py
+++ producers/models/turnstile.py
@@ -41,7 +41,7 @@ class Turnstile(Producer):
         
         
         super().__init__(
-            topic_name = f"com.streaming.produce.turnstile.{self.station.color.value}.{self.station.station_id}", # TODO: Come up with a better topic name
+            topic_name = f"com.streaming.produce.turnstile", # TODO: Come up with a better topic name
             key_schema = Turnstile.key_schema,
             value_schema = Turnstile.value_schema,
             num_partitions = 3,
