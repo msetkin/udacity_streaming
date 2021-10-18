3150c58351ccca7ce6083f4ca8544344607944bc
Mikhail Setkin <msetkin@yandex.ru>
2021-10-18 19:38:44 +0300

    done weapther/py

--- consumers/models/weather.py
+++ consumers/models/weather.py
@@ -15,9 +15,8 @@ class Weather:
 
     def process_message(self, message):
         """Handles incoming weather data"""
-        logger.info("weather process_message is incomplete - skipping")
-        #
-        #
-        # TODO: Process incoming weather messages. Set the temperature and status.
-        #
-        #
+        value = message.value()
+        self.temperature = value.get("temperature")
+        self.status = value.get("status")
+
+        logger.debug(f"weather message: {value}")
